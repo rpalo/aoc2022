317f2d8a98226f062b91f2397aa324834f3498ee
Ryan Palo <ryan@thepalos.com>
2022-12-01 21:29:12 -0800

    Finish day 2

--- crystal/spec/day2_spec.cr
+++ crystal/spec/day2_spec.cr
@@ -1,5 +1,40 @@
 require "./spec_helper"
 
 describe Day2 do
-  
+  describe "#score" do
+    it "scores ties" do
+      d = Day2.new
+      d.score('A', 'X').should eq(4)
+      d.score('B', 'Y').should eq(5)
+      d.score('C', 'Z').should eq(6)
+    end
+
+    it "scores victories" do
+      d = Day2.new
+      d.score('A', 'Y').should eq(8)
+      d.score('B', 'Z').should eq(9)
+      d.score('C', 'X').should eq(7)
+    end
+
+    it "scores losses" do
+      d = Day2.new
+      d.score('A', 'Z').should eq(3)
+      d.score('B', 'X').should eq(1)
+      d.score('C', 'Y').should eq(2)
+    end
+  end
+
+  describe "#sum_rounds" do
+    it "should add up the test file" do
+      d = Day2.new
+      d.sum_rounds("../data/day2_test.txt").should eq(15)
+    end
+  end
+
+  describe "#sum_rounds2" do
+    it "should add up the test file" do
+      d = Day2.new
+      d.sum_rounds2("../data/day2_test.txt").should eq(12)
+    end
+  end
 end
--- crystal/src/lib/day2.cr
+++ crystal/src/lib/day2.cr
@@ -1,12 +1,85 @@
-# Day 2: 
+# Day 2: Rock Paper Scissors
 # 
-# ...
+# Part 1:
+# Calculate scored rounds of rock paper scissors.
+# A and X = Rock, B and Y = Paper, C and Z = Scissors
+# 
+# Part 2:
+# X = Lose, Y = Tie, Z = Win
+# 
+# 3 points for tie, 6 points for win, 0 points for loss
+# 1 point for playing rock, 2 points for paper, 3 for scissors
 class Day2
-  def initialize
+  def sum_rounds(filename)
+    File.read_lines(filename).sum do |line|
+      score(line[0], line[2])
+    end
+  end
 
+  def sum_rounds2(filename)
+    File.read_lines(filename).sum do |line|
+      score2(line[0], line[2])
+    end
+  end
+
+  def score(opponent, you)
+    points = case you
+    when 'X'
+      1
+    when 'Y'
+      2
+    when 'Z'
+      3
+    else
+      0
+    end
+
+    if (
+      you == 'X' && opponent == 'A' ||
+      you == 'Y' && opponent == 'B' ||
+      you == 'Z' && opponent == 'C'
+    )
+      points += 3
+    elsif (
+      you == 'X' && opponent == 'C' ||
+      you == 'Y' && opponent == 'A' ||
+      you == 'Z' && opponent == 'B'
+    )
+      points += 6
+    end
+    points
+  end
+
+  def score2(opponent, outcome)
+    if outcome == 'X' # lose
+      if opponent == 'A'
+        3 # scissors, lose
+      elsif opponent == 'B'
+        1 # rock, lose
+      else
+        2 # paper, lose
+      end
+    elsif outcome == 'Y'  # tie
+      if opponent == 'A'
+        4 # rock, tie
+      elsif opponent == 'B'
+        5 # paper, tie
+      else
+        6 # scissors, tie
+      end
+    else  # win
+      if opponent == 'A'
+        8 # paper, win
+      elsif opponent == 'B'
+        9 # scissors, win
+      else
+        7 # rock, win
+      end
+    end
   end
   
   def main(filename)
-
+    puts "Part 1: #{sum_rounds(filename)}"
+    puts "Part 2: #{sum_rounds2(filename)}"
   end
 end
